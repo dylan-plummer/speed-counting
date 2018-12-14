48da724db9040946bb24f70d4020c11bf765513b
jumproper <dylanmplummer@gmail.com>
2018-12-14 14:03:22 -0500

    Add further visualizations

--- optical_flow.py
+++ optical_flow.py
@@ -2,7 +2,9 @@ import cv2
 import numpy as np
 from scipy import signal
 import matplotlib.pyplot as plt
-from repetition import wavelet_transform
+
+
+# from repetition import wavelet_transform
 
 
 def open_video(file):
@@ -29,67 +31,81 @@ def open_video(file):
     return buf
 
 
-def optical_flow(I1g, I2g, window_size, tau=1e-2):
-    kernel_x = np.array([[-1., 1.], [-1., 1.]])
-    kernel_y = np.array([[-1., -1.], [1., 1.]])
-    kernel_t = np.array([[1., 1.], [1., 1.]])  # *.25
-    w = window_size / 2  # window_size is odd, all the pixels with offset in between [-w, w] are inside the window
-    I1g = I1g / 255.  # normalize pixels
-    I2g = I2g / 255.  # normalize pixels
-    # Implement Lucas Kanade
-    # for each point, calculate I_x, I_y, I_t
-    mode = 'same'
-    fx = signal.convolve2d(I1g, kernel_x, boundary='symm', mode=mode)
-    fy = signal.convolve2d(I1g, kernel_y, boundary='symm', mode=mode)
-    ft = signal.convolve2d(I2g, kernel_t, boundary='symm', mode=mode) + \
-         signal.convolve2d(I1g, -kernel_t, boundary='symm', mode=mode)
+def high_pass_filter(img, dft_shift, filter=30):
+    rows, cols = img.shape
+    crow, ccol = rows // 2, cols // 2
+    fshift = np.copy(dft_shift)
+    fshift[crow - filter:crow + filter, ccol - filter:ccol + filter] = 0
 
-    u = np.zeros(I1g.shape)
-    v = np.zeros(I1g.shape)
-    # within window window_size * window_size
-    for i in range(w, I1g.shape[0] - w):
-        for j in range(w, I1g.shape[1] - w):
-            Ix = fx[i - w:i + w + 1, j - w:j + w + 1].flatten()
-            Iy = fy[i - w:i + w + 1, j - w:j + w + 1].flatten()
-            It = ft[i - w:i + w + 1, j - w:j + w + 1].flatten()
-            # b = ... # get b here
-            # A = ... # get A here
-            # if threshold τ is larger than the smallest eigenvalue of A'A:
-            nu = ...  # get velocity here
-            u[i, j] = nu[0]
-            v[i, j] = nu[1]
-
-    return u, v
+    # inverse DFT
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv2.idft(f_ishift)
+    mag, ang = cv2.cartToPolar(img_back[:, :, 0], img_back[:, :, 1])
+    return mag, ang
 
 
-video = open_video('test.mp4')
-print(video.shape)
+def low_pass_filter(img, dft_shift, filter=30):
+    rows, cols = img.shape
+    crow, ccol = rows // 2, cols // 2
+
+    # create a mask first, center square is 1, remaining all zeros
+    mask = np.zeros((rows, cols, 2), np.uint8)
+    mask[crow - filter:crow + filter, ccol - filter:ccol + filter] = 1
+
+    # apply mask and inverse DFT
+    fshift = dft_shift * mask
+    f_ishift = np.fft.ifftshift(fshift)
+    img_back = cv2.idft(f_ishift)
+    mag, ang = cv2.cartToPolar(img_back[:, :, 0], img_back[:, :, 1])
+    return mag, ang
 
 
 def fourier(video, i, j):
-    prev = cv2.cvtColor(video[i],cv2.COLOR_BGR2GRAY)
-    next = cv2.cvtColor(video[j],cv2.COLOR_BGR2GRAY)
+    prev = cv2.cvtColor(video[i], cv2.COLOR_BGR2GRAY)
+    next = cv2.cvtColor(video[j], cv2.COLOR_BGR2GRAY)
     flow = cv2.calcOpticalFlowFarneback(prev, next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
-    hsv = np.zeros_like(video[0])
-    hsv[...,1] = 255
-    mag, ang = cv2.cartToPolar(flow[...,0], flow[...,1])
-    hsv[...,0] = ang*180/np.pi/2
-    hsv[...,2] = cv2.normalize(mag,None,0,255,cv2.NORM_MINMAX)
-
     dft = cv2.dft(np.float32(flow), flags=cv2.DFT_COMPLEX_OUTPUT)
     dft_shift = np.fft.fftshift(dft)
-    #print(dft_shift.shape)
+    hsv = np.zeros_like(video[0])
+    hsv[..., 1] = 255
+    mag, ang = cv2.cartToPolar(dft_shift[..., 0], dft_shift[..., 1])
 
-    magnitude_spectrum = 20 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
 
+    # print('Fourier shape', dft_shift.shape)
+
+    #magnitude_spectrum = 255 * np.log(cv2.magnitude(dft_shift[:, :, 0], dft_shift[:, :, 1]))
+
+    '''
     plt.subplot(121), plt.imshow(mag, cmap='plasma')
-    plt.title('Input Image'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='plasma')
-    plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+    plt.title('mag'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(ang, cmap='plasma')
+    plt.title('ang'), plt.xticks([]), plt.yticks([])
     plt.savefig('frames/' + str(i) + '.png')
     plt.clf()
+    '''
+
+    lpf_mag, lpf_ang = low_pass_filter(prev, dft_shift, filter=30)
+    hpf_mag, hpf_ang = high_pass_filter(prev, dft_shift, filter=30)
+
+    intensity = np.median(hpf_ang)
+
+    plt.subplot(121), plt.imshow(lpf_ang, cmap='plasma')
+    plt.title('LPF'), plt.xticks([]), plt.yticks([])
+    plt.subplot(122), plt.imshow(hpf_ang, cmap='plasma')
+    plt.title('HPF'), plt.xticks([]), plt.yticks([])
+    plt.savefig('filters/' + str(i) + '.png')
+    plt.clf()
+
+    return intensity
 
 
-for i in range(0, len(video) - 2):
-    fourier(video, i, i + 2)
+video = open_video('noah.mp4')
+print(video.shape)
+signal = []
 
+for i in range(0, len(video) - 1):
+    intensity = fourier(video, i, i + 1)
+    signal.append(intensity)
+
+plt.plot(signal)
+plt.show()
